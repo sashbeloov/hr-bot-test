756b7c9c1c915d7bed488e64415d77a5b5ef0474
Aleksandr <beloovsasha23@gmail.com>
2025-03-15 10:46:34 +0500

    hr-bot
    hr-bot

---
+++ bot_test/lists.py
@@ -0,0 +1,49 @@
+
+sets = {
+    'Kombo set':["Fri kartoshkasiCoca-cola 0.5",55000,"images/komboset.jpg"],
+    '1+1 Sezar burger': ["1+1 Sezar burger",46000,"images/yengi-set.jpg"],
+    "Qiyqiriq set": 48000,
+    'Klassik set': 50000,
+    "Do'stlar 1x": 52000,
+    "Lester set": 53000,
+    "Snek set": 49000,
+    "Do'stlar 2x, achchiq": 54000,
+    "4 Friends Klassik burger": 56000,
+    "4 Friends Lester chiz": 57000,
+    "Do'stlar 4x, achchiq": 60000,
+    "1+1 Barbekyu burger": 47000,
+    "Yangi set": 58000,
+    "Skulll set": 59000,
+    "Do'stlar 1x, achchiq": 52000,
+    "Longer rings set": 51000,
+    "Big set": 65000,
+    "4 Friends Hot-dog": 43000,
+    "4 Friends Longer chiz": 44000,
+    "Do'stlar 4x": 62000
+}
+
+ichimliklar = {
+    'Montella 0.33': 15000,
+    "Qora choy": 16000,
+    "Ko'k choy": 17000,
+    "Limonli qora choy": 18000,
+    "Limonli ko'k choy": 19000,
+    'Espresso': 20000,
+    'Coca-cola 0.5': 21000,
+    "Fanta 0.5": 22000,
+    "Sprite 0.5": 23000,
+    "Les tea!": 24000,
+    "Frutea Berry mix": 25000,
+    'Frutea Orange': 26000,
+    'Frutea Strawberry': 27000,
+    "Americano": 28000,
+    "Ays kofe": 29000,
+    "Aysti": 30000,
+    "Cappuccino": 31000,
+    'Latte': 32000,
+    "Ays kofe milk": 33000,
+    "New Moxito": 34000,
+    "Berry Moxito": 35000
+}
+
+
